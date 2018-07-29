a7af33d33a97d025b69ec4e09c8589156087bf31
Silvio <silvio@sintuity.com>
2018-07-29 03:16:33 +0200

    Add image resize code

--- handler.js
+++ handler.js
@@ -1,3 +1,19 @@
+// @ts-check
+import processItem from './src/image'
+
+export async function processImage ({ Records: records }, context, callback) {
+  try {
+    await Promise.all(records.map(processItem))
+  } catch (error) {
+    console.error(error)
+    return callback(error, { records })
+  }
+
+  console.log(`Completed processing ${records.length} event${records.length === 1 ? '' : 's'}`)
+
+  return callback(null)
+}
+
 import sharp from "sharp";
 
 const done = body => {
---
+++ src/config.js
@@ -0,0 +1,5 @@
+import cfg from '../config.json'
+import imgcfg from '../imgconfig.json'
+
+export const config = cfg
+export const imgconfig = imgcfg
