04cbb5514efc8cbb34bb611e3e24e23822b5e13b
Silvio <silvio@i.graphics>
2018-07-29 16:37:53 +0200

    Remove unused ping function

--- handler.js
+++ handler.js
@@ -13,27 +13,3 @@ export async function processImage ({ Records: records }, context, callback) {
 
   return callback(null)
 }
-
-import sharp from "sharp";
-
-const done = body => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(body)
-  };
-};
-
-const fail = body => {
-  return {
-    statusCode: 500,
-    body: JSON.stringify(body)
-  };
-};
-
-export const ping = async (e, ctx, callback) => {
-  try {
-    callback(null, done({ pong: process.env.EXAMPLE }));
-  } catch (e) {
-    callback(fail(e));
-  }
-};
